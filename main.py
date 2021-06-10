1548196ecb6b2d0ed1e19950e253913129c39074
IrNoop4life <jesper@blaastudio.dk>
2021-06-10 13:23:29 +0200

    Update main.py
    Added Virustotal file check.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 import sys
 import datetime
+import time
 import hashlib
 import requests
 import argparse
 import json
 import logging
 import base64
+from termcolor import colored
 BANNER = """
 #######################################################
 #                                                     #
@@ -16,15 +18,20 @@ BANNER = """
 #######################################################
 
 """
+
+
+
 TODAY = datetime.datetime.today().strftime('%Y-%m-%d')
 LOG_FILE = "{}.log".format(TODAY)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
 AUTH_TOKEN = ""
+VT_API_KEY = ""
+DELAY = 10
 BASE_URL = "https://api.eu.xdr.trendmicro.com/beta/xdr/sandbox/"
 HEADERS = {
-  'Authorization': 'Bearer {}'.format(AUTH_TOKEN)
+    'Authorization': 'Bearer {}'.format(AUTH_TOKEN)
 }
 HEADERS_B = {
     'Content-Type': 'application/pdf',
@@ -32,9 +39,49 @@ HEADERS_B = {
 }
 PAYLOAD = {}
 
+
+def simple_vt_report(search):
+    vt_report = {}
+    vt = {}
+
+    vt_report = virustotal(search)
+    try:
+        if vt_report["response_code"] == 1:
+            vt = ({"scan_date": vt_report["scan_date"],
+                "positives": vt_report["positives"],
+                "total": vt_report["total"],
+                "sha256": vt_report["sha256"]
+                })
+        else:
+            vt = ({"message": "File hash not in VT."})
+    except:
+        logger.error("{} : {}".format(datetime.datetime.now(), vt_report))
+        vt = vt_report
+    return vt
+
+
+def virustotal(tosearch):
+    result = []
+
+    if VT_API_KEY:
+        params = {'apikey': VT_API_KEY, 'resource': tosearch}
+        headers = {
+            "Accept-Encoding": "gzip, deflate",
+            "User-Agent": "gzip,  Retro-Hunter"
+        }
+        response = requests.get('https://www.virustotal.com/vtapi/v2/file/report', params=params, headers=headers)
+        logger.info("{} : message: [{}].".format(datetime.datetime.now(), response.json()))
+        return response.json()
+    else:
+        logger.error("{} : message: Please check your Virustotal API key.".format(datetime.datetime.now()))
+        return {}
+
+
 def encode(string):
     ret = base64.b64encode(string.encode("ascii"))
     return ret
+
+
 def file_submit(fname, documentPassword, archivePassword):
     if documentPassword:
         PAYLOAD = {'documentPassword': encode(documentPassword)}
@@ -49,15 +96,16 @@ def file_submit(fname, documentPassword, archivePassword):
     response = requests.request("POST", url, headers=HEADERS, data=PAYLOAD, files=files)
     ret = json.loads(response.text)
     print(json.dumps(ret, indent=4, sort_keys=False))
-    logger.info("{} : {}".format(datetime.datetime.now(),json.dumps(ret, indent=4, sort_keys=False)))
+    logger.info("{} : {}".format(datetime.datetime.now(), json.dumps(ret, indent=4, sort_keys=False)))
+
 
 def check_state(taskID):
-
     url = "{}tasks/{}".format(BASE_URL, taskID)
     response = requests.request("GET", url, headers=HEADERS, data=PAYLOAD)
     ret = json.loads(response.text)
     print(json.dumps(ret, indent=4, sort_keys=False))
-    logger.info("{} : {}".format(datetime.datetime.now(),json.dumps(ret, indent=4, sort_keys=False)))
+    logger.info("{} : {}".format(datetime.datetime.now(), json.dumps(ret, indent=4, sort_keys=False)))
+
 
 def get_vaReport(reportId):
     rname = "VAReport-{}.pdf".format(reportId)
@@ -68,6 +116,7 @@ def get_vaReport(reportId):
     logger.info("{} : {} downloaded!".format(datetime.datetime.now(), rname))
     print("{} downloaded!".format(rname))
 
+
 def get_invP(reportId):
     rname = "investigationPackage-{}.zip".format(reportId)
     url = "{}reports/{}?type=investigationPackage".format(BASE_URL, reportId)
@@ -77,19 +126,40 @@ def get_invP(reportId):
     logger.info("{} : {} downloaded!".format(datetime.datetime.now(), rname))
     print("{} downloaded!".format(rname))
 
+
 def get_so(reportId):
+    dot = ""
     url = "{}reports/{}?type=suspiciousObject".format(BASE_URL, reportId)
     response = requests.request("GET", url, headers=HEADERS_B, data=PAYLOAD)
     ret = json.loads(response.text)
     print(json.dumps(ret, indent=4, sort_keys=False))
-    logger.info("{} : {}".format(datetime.datetime.now(),json.dumps(ret, indent=4, sort_keys=False)))
+    logger.info("{} : {}".format(datetime.datetime.now(), json.dumps(ret, indent=4, sort_keys=False)))
+    print(colored('// Checking Suspicious objects against Threat intel.', attrs=['bold']))
+    if not VT_API_KEY:
+        print(colored("{'message': 'ERROR!', 'reason': 'No Virustotal API key found.'}", 'red'))
+        sys.exit(0)
+    try:
+        for _s in ret['data']:
+            if _s['type'] == "sha1":
+                dot = dot + "."
+                print("Checking File hash", colored('[{}]'.format(_s['value']), 'yellow'), "against Virustotal. Hang on{}".format(dot))
+                time.sleep(DELAY)
+                try:
+                    print(colored('{}'.format(simple_vt_report(_s['value'])), 'white'))
+                except Exception as e:
+                    print(colored("{'message': 'Did not get any result from VT.', 'reason': 'Depending on the no. of requests, we might have hit a timeout.', 'logfile': "+LOG_FILE+"}", 'red'))
+                    logger.error("{} : {}".format(datetime.datetime.now(), e))
+    except Exception as ex:
+        logger.error("{} : {}".format(datetime.datetime.now(), ex))
+        pass
+
 
 def get_quota():
     url = "{}quota".format(BASE_URL)
     response = requests.request("GET", url, headers=HEADERS_B, data=PAYLOAD)
     ret = json.loads(response.text)
     print(json.dumps(ret, indent=4, sort_keys=False))
-    logger.info("{} : {}".format(datetime.datetime.now(),json.dumps(ret, indent=4, sort_keys=False)))
+    logger.info("{} : {}".format(datetime.datetime.now(), json.dumps(ret, indent=4, sort_keys=False)))
 
 
 if __name__ == '__main__':
@@ -139,4 +209,3 @@ if __name__ == '__main__':
 
     if args.quota:
         get_quota()
-
